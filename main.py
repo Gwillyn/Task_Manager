56bf3d902d35ad587bdfe7f56e576e2b90ef24b8
gwillyn <gwillynd@gmail.com>
2026-04-14 17:18:41 -0400

    Tkinter edits
    Added Frames for the different sections of the home screen. Added
    buttons for Task Frame and Labels for all Frames.

--- main.py
+++ main.py
@@ -3,18 +3,54 @@ from tkinter import ttk
 
 
 def main():
+    text_font = "JetBrainsMono Nerd Font"
+
     root = tk.Tk()
     root.title("Task Manager")
     root.geometry("600x600")
 
-    frame = ttk.Frame(root, padding=(3, 3, 12, 12))
-    frame.grid(column=0, row=0, sticky=("nwes"))
-
     root.columnconfigure(0, weight=1)
     root.rowconfigure(0, weight=1)
 
-    frame.columnconfigure(2, weight=1)
+    # Main Frame
+    mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+    mainframe.grid(column=0, row=0, sticky=("nwes"))
 
+    mainframe.columnconfigure(0, weight=1)
+
+    # Header Frame
+    headerframe = ttk.Frame(mainframe)
+    headerframe.grid(column=0, row=0, sticky="ew")
+    headerframe.columnconfigure(0, weight=1)
+
+    ttk.Label(
+        headerframe, text="Task Manager", font=(text_font, 20, "bold"), anchor="center"
+    ).grid(column=0, row=0, pady=10)
+
+    # Task Area
+
+    taskframe = ttk.Frame(mainframe)
+    taskframe.grid(column=0, row=1, sticky="ew")
+    taskframe.columnconfigure(0, weight=1)
+    taskframe.rowconfigure(0, weight=1)
+
+    buttonframe = ttk.Frame(taskframe)
+    buttonframe.grid(column=0, row=0, sticky="ew")
+
+    settings_button = ttk.Button(buttonframe, text="Settings")
+    settings_button.grid(column=0, row=0, padx=15)
+
+    make_task_button = ttk.Button(buttonframe, text="Make Task")
+    make_task_button.grid(column=1, row=0, padx=15)
+
+    # List Area
+    listframe = ttk.Frame(mainframe)
+    listframe.grid(column=0, row=2, sticky="ew")
+    listframe.columnconfigure(0, weight=1)
+
+    ttk.Label(listframe, text="Tasks: ", font=(text_font, 16, "bold")).grid(
+        column=0, row=0, pady=10
+    )
     root.mainloop()
 
 
